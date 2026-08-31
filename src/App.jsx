d73d40439eb3dd2639efbe26fb8f85bc577c22fd
Livewirenbs <livewiresalem567@gmaiol.com>
2026-08-31 16:43:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Learn_react from './components/Learn_react'
+
+function App() {
+  return (
+    <>
+      <h1>Praveen Kumar</h1>
+      <div>
+        <Learn_react />
+      </div>
+    </>
+  )
+}
+
+export default App
